4932e05209af5cced02014531e9620e79a6d8eae
张迪 <codeway3@126.com>
2017-04-24 01:10:48 +0800

    encapsulation

--- comments_crawler.py
+++ comments_crawler.py
@@ -1,25 +1,39 @@
 import json
 import requests
 
+
+class CommentsCrawler(object):
+    def __init__(self):
+        self.offset = 0
+        self.newListSize = 1
+        self.results = []
+        self.model = 'http://comment.news.163.com/api/v1/products/a2869674571f77b5a0867c3d71db5856/threads/{}/comments/newList?offset={}&limit=30&showLevelThreshold=72&headLimit=1&tailLimit=2&callback=getData&ibc=newspc'
+
+    def getId(self, newsId):
+        self.newsId = newsId
+
+    def getComments(self):
+        while self.offset < self.newListSize:
+            url = self.model.format(self.newsId, self.offset)
+            r = requests.get(url)
+            data = r.text
+            data = data[8:]
+            data = data[:-3]
+            j = json.loads(data)
+            self.newListSize = j['newListSize']
+            commentIds = j['commentIds']
+            comments = j['comments']
+            for commentId in commentIds:
+                commentId = commentId.split(',')
+                comment = comments[commentId[-1]]
+                self.results.append(comment['content'])
+            self.offset += 30
+            print('{} {}'.format(self.newListSize, self.offset))
+
+
 if __name__ == '__main__':
-    results = []
+    crawler = CommentsCrawler()
     newsId = 'CIKJGSUN0001875N'
-    offset = 0
-    newListSize = 1
-    while offset < newListSize:
-        url = 'http://comment.news.163.com/api/v1/products/a2869674571f77b5a0867c3d71db5856/threads/{}/comments/newList?offset={}&limit=30&showLevelThreshold=72&headLimit=1&tailLimit=2&callback=getData&ibc=newspc'.format(newsId, offset)
-        r = requests.get(url)
-        data = r.text
-        data = data[8:]
-        data = data[:-3]
-        j = json.loads(data)
-        newListSize = j['newListSize']
-        commentIds = j['commentIds']
-        comments = j['comments']
-        for commentId in commentIds:
-            commentId = commentId.split(',')
-            comment = comments[commentId[-1]]
-            results.append(comment['content'])
-        offset += 30
-        print('{} {}'.format(newListSize, offset))
-    print(results)
+    crawler.getId(newsId)
+    crawler.getComments()
+    print(crawler.results)
