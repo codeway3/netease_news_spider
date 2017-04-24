eff26c80f571406b4de974848cdc160fff678dbb
张迪 <codeway3@126.com>
2017-04-24 21:24:30 +0800

    re-encapsulation

--- comments_crawler.py
+++ comments_crawler.py
@@ -1,18 +1,17 @@
+import sys
 import json
 import requests
 
 
 class CommentsCrawler(object):
-    def __init__(self):
+    def _set_id(self, newsId):
         self.offset = 0
         self.newListSize = 1
         self.results = []
         self.model = 'http://comment.news.163.com/api/v1/products/a2869674571f77b5a0867c3d71db5856/threads/{}/comments/newList?offset={}&limit=30&showLevelThreshold=72&headLimit=1&tailLimit=2&callback=getData&ibc=newspc'
-
-    def getId(self, newsId):
         self.newsId = newsId
 
-    def getComments(self):
+    def _get_comments(self):
         while self.offset < self.newListSize:
             url = self.model.format(self.newsId, self.offset)
             r = requests.get(url)
@@ -28,12 +27,23 @@ class CommentsCrawler(object):
                 comment = comments[commentId[-1]]
                 self.results.append(comment['content'])
             self.offset += 30
-            print('{} {}'.format(self.newListSize, self.offset))
+            if self.offset > self.newListSize:
+                self.offset = self.newListSize
+            sys.stdout.write('\r共{}条评论，正在爬取第{}条'.format(self.newListSize, self.offset))
+        sys.stdout.write('\n')
+        # sys.stdout.flush()
+
+    def _print_results(self):
+        print('编号{}新闻爬取完毕！'.format(self.newsId))
+        print('sample: ', self.results[:10])
+
+    def craw(self, newsId):
+        self._set_id(newsId)
+        self._get_comments()
+        self._print_results()
 
 
 if __name__ == '__main__':
     crawler = CommentsCrawler()
     newsId = 'CIKJGSUN0001875N'
-    crawler.getId(newsId)
-    crawler.getComments()
-    print(crawler.results)
+    crawler.craw(newsId)
